ea49ba907e1de356a3f1b2f18fc302d1d4835cb1
SlaVcE <102253539+SlaVcE14@users.noreply.github.com>
2024-11-09 18:15:11 +0100

    Cleanup

--- Domasna 1/src/main/java/mk/ukim/finki/Main.java
+++ Domasna 1/src/main/java/mk/ukim/finki/Main.java
@@ -9,16 +9,13 @@ import org.jsoup.select.Elements;
 
 import java.io.*;
 import java.net.URI;
-import java.net.URL;
-import java.net.URLConnection;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.text.DecimalFormat;
-import java.time.LocalDate;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoField;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
@@ -28,7 +25,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
-    //https://www.mse.mk/mk/stats/symbolhistory/stb?FromDate=30.7.2024&ToDate=31.10.2024
     public static final String URL = "https://www.mse.mk/mk/stats/symbolhistory/";
     public static final String FILE_NAME = "data.json";
 
@@ -39,7 +35,7 @@ public class Main {
     }
 
     public Main() throws Exception {
-        //Read current base
+        //Read current database
         issuers = ReadData();
 
         int total = 0;
@@ -92,15 +88,15 @@ public class Main {
 
                         if (!String.format("%02d.%02d.%d", day, month, year).equals(date) && (month == fromMonth && day > fromDay)) {
                             List<StockData> data = getData(code, String.format("FromDate=%d.%d.%d&ToDate=%d.%d.%d", fromDay, fromMonth, fromYear, day, month, year));
-                            issuer.getStockDataList().addAll(0, data); // Ensure no duplicates
+                            issuer.getStockDataList().addAll(0, data);
                         }
                     }
                 } catch (Exception e) {
-                    e.printStackTrace(); // Handle exceptions for each task
+                    e.printStackTrace();
                 }
             });
         }
-        
+
         executor.shutdown();
         try {
             if (!executor.awaitTermination(600, TimeUnit.SECONDS)) {
@@ -231,24 +227,7 @@ public class Main {
                 .GET()
                 .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        String body = response.body();
-        return body;
-/*
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(new URI(URL + params))
-                .GET()
-                .build();
-
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-        if (response.statusCode() == 200) {
-            return response.body();
-        } else {
-            System.err.println("Failed to fetch HTML, HTTP response code: " + response.statusCode());
-            return "";
-        }
-*/
+        return response.body();
     }
 
     private List<String> getAllIssuers() throws Exception {
